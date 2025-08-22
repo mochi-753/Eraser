96c0d517eb5b39cb611910755a081e95a9855e62
mochi-753 <naka.mochi11@gmail.com>
2025-08-22 23:18:26 +0900

    高級消しゴムを追加

--- src/main/java/com/mochi_753/eraser/register/ModItems.java
+++ src/main/java/com/mochi_753/eraser/register/ModItems.java
@@ -3,6 +3,7 @@ package com.mochi_753.eraser.register;
 import com.mochi_753.eraser.Eraser;
 import com.mochi_753.eraser.item.EraserItem;
 import com.mochi_753.eraser.item.TestItem;
+import com.mochi_753.eraser.item.UltraEraserItem;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
@@ -15,6 +16,8 @@ public class ModItems {
 
     public static final RegistryObject<Item> ERASER = ITEMS.register("eraser",
             () -> new EraserItem(new Item.Properties().stacksTo(1).rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> ULTRA_ERASER = ITEMS.register("ultra_eraser",
+            () -> new UltraEraserItem(new Item.Properties().stacksTo(1).rarity(Rarity.EPIC)));
     public static final RegistryObject<Item> TEST = ITEMS.register("test",
             () -> new TestItem(new Item.Properties().stacksTo(1).rarity(Rarity.EPIC)));
 
