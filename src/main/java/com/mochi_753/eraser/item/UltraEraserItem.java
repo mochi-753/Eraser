7c96e78a5909c5aa48edb93550f69cd8d9580a1e
mochi-753 <naka.mochi11@gmail.com>
2025-08-23 17:11:38 +0900

    高級消しゴムが不良品だったので修正

--- src/main/java/com/mochi_753/eraser/item/UltraEraserItem.java
+++ src/main/java/com/mochi_753/eraser/item/UltraEraserItem.java
@@ -1,6 +1,7 @@
 package com.mochi_753.eraser.item;
 
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -22,6 +23,7 @@ public class UltraEraserItem extends EraserItem {
                 erasePlayer(player, targetPlayer, targetServerPlayer);
             }
         } else {
+            target.remove(Entity.RemovalReason.DISCARDED);
             forceErase(target, player);
         }
     }
