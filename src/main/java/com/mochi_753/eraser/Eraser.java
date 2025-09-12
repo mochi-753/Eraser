e6fdd10ee7d24c329c6f07efac99096cecafb82c
mochi-753 <naka.mochi11@gmail.com>
2025-09-12 18:34:47 +0900

    v1.10.0

--- src/main/java/com/mochi_753/eraser/Eraser.java
+++ src/main/java/com/mochi_753/eraser/Eraser.java
@@ -5,11 +5,9 @@ import com.mochi_753.eraser.network.EraserNetwork;
 import com.mochi_753.eraser.register.EraserItems;
 import com.mochi_753.eraser.register.EraserTabs;
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
@@ -22,14 +20,10 @@ public class Eraser {
         IEventBus bus = context.getModEventBus();
 
         context.registerConfig(ModConfig.Type.COMMON, EraserConfig.COMMON_SPEC);
-        MinecraftForge.EVENT_BUS.addListener(this::commonSetup);
+        EraserNetwork.init();
         EraserItems.register(bus);
         EraserTabs.register(bus);
 
         new EraserTConstruct(bus, context);
-    }
-
-    private void commonSetup(FMLCommonSetupEvent event) {
-        EraserNetwork.init();
     }
 }
